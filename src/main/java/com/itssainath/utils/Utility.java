9974e2419283d79160da50fc0f547cbe94658d5c
Sainath <itssainath@xyz.com>
2022-07-02 16:50:59 +0530

    Sending Simple Asynchronous Request to BolckChain

--- src/main/java/com/itssainath/utils/Utility.java
+++ src/main/java/com/itssainath/utils/Utility.java
@@ -6,6 +6,7 @@ import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 public class Utility {
 
@@ -13,6 +14,15 @@ public class Utility {
 
         Web3j web3j = Web3j.build(new HttpService(CredDetails.NODE_RPC_URL));
         Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
-        return "Web3ClientVersion : " + web3ClientVersion.getWeb3ClientVersion();
+        return "Web3ClientVersion : " + web3ClientVersion.getWeb3ClientVersion() + " : Sync";
+    }
+
+    public String sendingAsynchronousRequest() throws IOException {
+
+        Web3j web3j = Web3j.build(new HttpService(CredDetails.NODE_RPC_URL));
+        CompletableFuture<Web3ClientVersion> web3ClientVersionCompletableFuture =
+                web3j.web3ClientVersion().sendAsync();
+
+        return "Web3ClientVersion : " + web3ClientVersionCompletableFuture.join().getWeb3ClientVersion() + " : Sync";
     }
 }
